37f21457dc75c22a8f1ed302091ba0840f808b66
Mohammed Ammar <imohammedomz@gmail.com>
2024-08-15 12:25:33 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Domzi",
+  title: "Mohammed Portfolio",
   description: "Mohammed Alammar Portfolio",
 };
 
